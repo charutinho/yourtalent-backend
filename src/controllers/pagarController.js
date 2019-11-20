b960a6772d4ee64624babed31f0ba8a6bb0dd2bb
Charutinho <alissoncf8@gmail.com>
2019-11-19 21:21:16 -0300

    Conserto das rotas 'routes' para 'router' pois a mesma não existia

--- src/controllers/pagarController.js
+++ src/controllers/pagarController.js
@@ -50,15 +50,15 @@ router.get('/checkout/:id/:email', async (req, res) => {
     }
 })
 
-routes.get('/success', (req, res) => {
+router.get('/success', (req, res) => {
     return res.render('success_screen')
 })
 
-routes.get('/pending', (req, res) => {
+router.get('/pending', (req, res) => {
     return res.render('pending_screen')
 })
 
-routes.get('/failure', (req, res) => {
+router.get('/failure', (req, res) => {
     return res.render('failure_screen')
 })
 
