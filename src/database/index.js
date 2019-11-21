93f6a2737ae4400e4753632172375437680509ac
Charutinho <alissoncf8@gmail.com>
2019-11-21 14:03:48 -0300

    MongoDB agora está hospedado no MongoDB Atlas

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Conexão com mongodb
-const uri = 'mongodb://localhost/yourtalent'
+const uri = 'mongodb+srv://admin:ytadmin777@yourtalent-c3na1.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
