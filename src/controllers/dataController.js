ea465d51ddd0500d1d3e98d6f3b40a83a7436753
Charutinho <alissoncf8@gmail.com>
2019-11-27 11:32:51 -0300

    Rota para alterar senha

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 const Post = require('../models/post');
@@ -7,7 +8,7 @@ const Camp = require('../models/camp');
 const router = express.Router();
 
 router.get('/data/:id', async (req, res) => {
-    
+
     const idUser = req.params.id;
 
     const user = await User.findOne({ _id: idUser });
@@ -26,7 +27,7 @@ router.post('/listData', async (req, res) => {
 
     const { nivel } = req.body;
 
-    const user = await User.find({ nivel: 1 });    
+    const user = await User.find({ nivel: 1 });
 
     user.senha = undefined;
 
@@ -38,7 +39,7 @@ router.post('/listData', async (req, res) => {
 router.post('/updateData', async (req, res) => {
 
     console.log(req.body);
-    
+
     const id = req.body.id;
     const nome = req.body.nome;
 
@@ -47,18 +48,32 @@ router.post('/updateData', async (req, res) => {
         return res.send({
             message: `Usuário ${nome} atualizado com sucesso!`
         })
-    } catch (err){
-        if(err){
+    } catch (err) {
+        if (err) {
             return res.status(400).send({ error: 'Algo de errado ao atualizar as informações' })
         }
     }
 
 });
 
-router.get('/deleteuser/:id', async(req, res) => {
+router.get('/deleteuser/:id', async (req, res) => {
     await Post.deleteMany({ autor: req.params.id });
     await Camp.deleteMany({ idParticipante: req.params.id })
     await User.findByIdAndDelete(req.params.id);
+})
+
+router.post('/novasenha', async (req, res) => {
+    const { email, senha, novasenha } = req.body;
+    const user = await User.findOne({ email }).select('+senha')
+    if (!await bcrypt.compare(senha, user.senha)) {
+        return res.send({ message: 'Senha incorreta' })
+    }
+
+    const hash = await bcrypt.hash(novasenha, 15);
+    const novasenhausuario = hash;
+
+    await User.findByIdAndUpdate(req.body.idUser, { $set: { senha: novasenhausuario } })
+    return res.send({ message: 'Sucesso' })
 })
 
 
